7044d0072569ca382841bbd3d8300571d76d4646
Lewis Moten <lewismoten@gmail.com>
2024-06-04 02:44:06 -0400

    update/load data

--- file.js
+++ file.js
@@ -4,29 +4,56 @@ var config = require('./config');
 function validate_file_name(file_name) {
   var patt = /^[0-9a-zA-Z-_]+$/;
   if (!patt.test(file_name)) {
-    throw new Error("File name can contain alphabets, numbers, hyphen or underscore");
+    config.logger.error(`Invalid file name '${file_name}'`);
+    throw new Error("File name can contain letters, numbers, hyphen or underscore");
   }
 }
 
 function readFolder(path, cb) {
   fs.readdir(path, function (err, files) {
-    if (err) {
-      throw err;
-    }
-
-    files = files.filter(file => config.script_pattern.test(file));
-
-    var schemaPath = files.indexOf("schema.sql");
-    if (schemaPath > -1) {
-      files.splice(schemaPath, 1);
-    }
+    if (err) throw err;
+    files = files
+      .filter(file => config.script_pattern.test(file))
+      .filter(file => file !== 'schema.sql' && file !== 'data.sql');
     cb(files);
   });
 }
 
+function readMigrations(path, after_timestamp, max_count, cb) {
+  var file_paths = [];
+  var timestamps = [];
+  readFolder(path, function (files) {
+    files.forEach(function (file_path) {
+      const match = /^(\d+)\_/.exec(file_path);
+      if (match && match.length > 1) {
+        const timestamp = parseInt(match[1], 10);
+        if (match[1].length !== 13) {
+          config.logger.warn(`Invalid timestamp: ${file_path}`);
+        } else if (timestamps.includes(timestamp)) {
+          config.logger.warn(`Ignoring duplicate timestamp: ${file_path}`);
+        } else if (timestamp <= after_timestamp) {
+          config.logger.debug(`Skipping: ${file_path}`);
+          // already migrated
+          return;
+        } else {
+          file_paths.push({ timestamp, file_path });
+        }
+      }
+    });
+    file_paths.sort(compareTimestamps);
+    if (max_count > -1) {
+      file_paths = file_paths.slice(0, max_count);
+    }
+    cb(file_paths);
+  });
+}
+
+const compareTimestamps = ({ timestamp: a }, { timestamp: b }) => a - b;
+
 function readFile(path, cb) {
   fs.readFile(path, function (err, data) {
     if (err) {
+      config.logger.error(`Unable to read file '${path}`);
       throw err;
     }
 
@@ -37,5 +64,6 @@ function readFile(path, cb) {
 module.exports = {
   validate_file_name: validate_file_name,
   readFolder: readFolder,
-  readFile: readFile
+  readFile: readFile,
+  readMigrations
 };
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 var migrations = require('../index');
 var testCommons = require('./test_commons');
-var mysql = require('./mysql');
+var poolManager = require('./poolManager');
 var assert = require('assert');
 var fs = require('fs');
 var config = require('../config');
 var logger = require('../logger');
+var queryFunctions = require('../query');
 
 var path = __dirname + '/migrations';
 
@@ -19,21 +20,17 @@ describe('index.js', function () {
     done();
   });
 
-  context('init', function () {
+  context('readOptions', function () {
     it('reads template option', function (done) {
       var name = __dirname + '/migrations/template_17c2387b-38af-4ef6-bf6f-bb72f68255ff.js';
       fs.writeFileSync(name, 'this is a template', { encoding: 'utf-8' });
-      migrations.init(
-        mysql,
-        path,
-        function () {
-          assert.equal(name, config.template);
-          done();
-        },
+      migrations.readOptions(
         [
           `--template ${name}`
         ]
       );
+      assert.equal(name, config.template);
+      done();
     })
     it('assigns --logger to config.logger', function (done) {
       function noop() { };
@@ -48,33 +45,74 @@ describe('index.js', function () {
       };
       assert.notEqual(config.logger, newLogger);
 
-      migrations.init(
-        mysql,
-        path,
-        function () {
-          assert.equal(config.logger, newLogger);
-          done();
-        },
+      migrations.readOptions(
         [
           '--logger',
           newLogger
         ]
       );
+      assert.equal(config.logger, newLogger);
+      done();
     });
     it('assigns --log-level to config.logLevel', function (done) {
       var newLevel = 'NONE';
       assert.notEqual(config.logLevel, newLevel);
-      migrations.init(
-        mysql,
-        path,
-        function () {
-          assert.equal(config.logLevel, newLevel);
-          done();
-        },
+      migrations.readOptions(
         [
           `--log-level ${newLevel}`
         ]
       );
+      assert.equal(config.logLevel, newLevel);
+      done();
+    });
+  });
+  context('init', function () {
+
+    it('exports data with --update-data', function (done) {
+      const timestamp = Date.now();
+      var pool = poolManager.newPool();
+      pool.getConnection(function (err, connection) {
+        if (err) throw err;
+        // ensure we have at least 1 migration
+        queryFunctions.updateRecords(pool, 'up', timestamp, function () {
+          const testId = 'dc00798c-f149-444d-8c49-1d9469b875cf';
+          // add data to export
+          connection.query(`
+create table if not exists Foo ( bar varchar(36) );
+insert into Foo (bar) values ('${testId}');
+`, function (err, res) {
+            if (err) {
+              throw err;
+            }
+
+            migrations.init(
+              pool,
+              path,
+              function () {
+                pool.end();
+                const fileExists = fs.existsSync(`${path}/data.sql`);
+                assert.ok(fileExists, 'data.sql must exist');
+
+                const content = fs.readFileSync(`${path}/data.sql`, { encoding: 'utf-8' });
+
+                assert.ok(content.includes('INSERT INTO `Foo`'), 'Must include insert statement');
+                assert.ok(content.includes(testId), 'Must include data');
+
+                assert.ok(!content.includes('CREATE TABLE `Foo`'), 'Must not include create table statement');
+
+                assert.ok(!content.includes(`INSERT INTO \`${config.table}\``), 'Must not populate migrations');
+
+                done();
+              },
+              [
+                '--update-data',
+                '--argv',
+                ['', '', 'up']
+              ]
+            );
+          })
+        });
+      });
     });
   });
 
